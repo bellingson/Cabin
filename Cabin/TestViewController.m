89579e99659393337e07fe7c65431f31196ae0cb
bellingson <ben@nofluffjuststuff.com>
2014-10-15 12:24:15 -0400

    v 1.0.4 updates

--- Cabin/TestViewController.m
+++ Cabin/TestViewController.m
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 
 #define TEST_SAMPLE_SIZE 10
+//#define TEST_SAMPLE_SIZE 200
 
 @interface TestViewController ()
 
@@ -253,7 +254,7 @@
     [self showImages];
 
     
-    //double delayInSeconds = 2.0;
+    //double delayInSeconds = 10.0;
     double delayInSeconds = 0.1;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
